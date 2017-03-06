409aa5d2b14635ff5aea22ba27defff2c8ec77aa
Dario Arias <h.dario95@gmail.com>
2017-03-06 16:12:39 -0500

    Build

--- AplicacionPrueba/AplicacionPrueba/src/app/services/master-url.service.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/services/master-url.service.ts
@@ -5,7 +5,7 @@ export class MasterURlService {
   private _url:string;
 
   constructor() {
-    this._url = "http://localhost:1337/";
+    this._url = "https://examen-twj-ariashomero-homero.c9users.io/";
     // this.url = "https://aplicacion-adrianeguez.c9users.io"
     // this.url = "https://asdasd-asdasdasd.herokuapp.com"
   }
